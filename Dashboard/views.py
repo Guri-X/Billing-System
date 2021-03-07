def28a49d0776788e1e32ecf49cf0f35f79445f6
Guri99 <singhhh890.gs@gmail.com>
2021-03-07 18:14:55 +0530

    Updated UI

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.contrib.auth import logout
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
 
 class AddProductView(TemplateView):
     template_name = 'addproduct.html'
+
+def logoutHandle(request):
+    logout(request)
+    return redirect('/')
--- Login/views.py
+++ Login/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 class LoginPage(TemplateView):
     template_name = 'login.html'
 
-@method_decorator(login_required, name='dispatch')
 class SessionPage(TemplateView):
     template_name = 'logged_in.html'
