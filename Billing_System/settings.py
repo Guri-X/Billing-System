370152295f1ffbd44597036545ddbab2bee9406a
Xan0er <singhhh890.gs@gmail.com>
2021-12-26 17:34:06 +0530

    fixed front end and added mysql as databases

--- Billing_System/settings.py
+++ Billing_System/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 STATICFILES_DIRS = [
-    "/home/xan0er/Django/static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Application definition
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'Billing_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'billing_system',
+        'USER': 'g_dev',
+        'PASSWORD': 'UvsSoXG9UWPe46',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
